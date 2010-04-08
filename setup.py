1ca2e29f77ee3c7592126eaf2d7657ea7e5a89d4
Taylor Mitchell <taylor.mitchell@gmail.com>
2010-04-08 11:28:34 -0700

    Updates setup.py for distro

--- setup.py
+++ setup.py
@@ -1,13 +1,32 @@
+import os
 from setuptools import setup, find_packages
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
     name = "django-wiki",
-    version = "1.1",
+    version = "1.1dev",
     url = 'http://github.com/tmitchell/django-wiki',
     license = 'BSD',
     description = "A super simple wiki for Django.",
-    author = 'Taylor Mitchell',
+    long_description = read('README'),
+
+    author = 'John Sutherland, Taylor Mitchell',
+    author_email = "taylor.mitchell@gmail.com",
+
     packages = find_packages('src'),
     package_dir = {'': 'src'},
+    
     install_requires = ['setuptools'],
+
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+    ]
 )

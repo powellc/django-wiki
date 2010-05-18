4f4f86151d09abbfce85caf45ec63bf1d0f5938c
Taylor Mitchell <taylor.mitchell@gmail.com>
2010-05-17 23:45:27 -0700

    Adds support for customizing the WIKI_WORD via settings

--- src/wiki/templatetags/wiki.py
+++ src/wiki/templatetags/wiki.py
@@ -1,19 +1,20 @@
 import re
 
 from django import template
-
-
-WIKI_WORD = r'(?:[A-Z]+[a-z]+){2,}'
-
+from django.conf import settings
 
 register = template.Library()
 
-
-wikifier = re.compile(r'\b(%s)\b' % WIKI_WORD)
-
+# If WIKI_WORD is specified in settings, use that regex
+# otherwise use the WikiWord format 
+try:
+    WIKI_WORD = settings.WIKI_WORD
+except AttributeError:
+    WIKI_WORD = r'(?:[A-Z]+[a-z]+){2,}'
 
 @register.filter
 def wikify(s):
+    wikifier = re.compile(r'\b(%s)\b' % WIKI_WORD)
     from django.core.urlresolvers import reverse
     wiki_root = reverse('wiki.views.index', args=[], kwargs={})
     return wikifier.sub(r'<a href="%s\1/">\1</a>' % wiki_root, s)

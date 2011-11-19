793227feb743373eba7b32959982d7ec78f48420
Colin Powell <colin.powell@gmail.com>
2011-11-18 23:43:49 -0500

    Adding a de-camel-case filter for displaying wiki pages nicely.

--- wiki/templatetags/wiki.py
+++ wiki/templatetags/wiki.py
@@ -1,4 +1,5 @@
 import re
+from enum import Enum
 
 from django import template
 from django.conf import settings
@@ -18,3 +19,25 @@ def wikify(s):
     from django.core.urlresolvers import reverse
     wiki_root = reverse('wiki.views.index', args=[], kwargs={})
     return wikifier.sub(r'<a href="%s\1/">\1</a>' % wiki_root, s)
+
+class DeCamelCaseMethods(Enum):
+    default = 2**0
+    force_lower_case = 2**1
+
+@register.filter
+def de_camel_case(stringAsCamelCase,delim=' ',method=DeCamelCaseMethods.default):
+    """Adds spaces to a camel case string.  Failure to space out string returns the original string.
+    >>> space_out_camel_case('DMLSServicesOtherBSTextLLC')
+    'DMLS Services Other BS Text LLC'
+    """
+    import re
+    
+    if stringAsCamelCase is None:
+        return None
+
+    normalize = lambda s:s
+    if (method == DeCamelCaseMethods.force_lower_case):
+        normalize = lambda s:s.lower()
+    
+    pattern = re.compile('([A-Z][A-Z][a-z])|([a-z][A-Z])')
+    return normalize(pattern.sub(lambda m: m.group()[:1] + delim + m.group()[1:], stringAsCamelCase))
